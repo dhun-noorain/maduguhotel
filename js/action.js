50d4bbc81fe20f84d2b5c4fde9ce713876bc944d
noorain <harunaumar109@gmail.com>
2023-09-03 08:41:03 +0100

    update reservation form validation

--- js/action.js
+++ js/action.js
@@ -11,8 +11,6 @@ const checkOut = reserve_form.elements["checkOut"];
 name.addEventListener("keyup", check, false);
 email.addEventListener("keyup", check, false);
 phone.addEventListener("keyup", check, false);
-checkIn.addEventListener("keyup", check, false);
-checkOut.addEventListener("keyup", check, false);
 reserve_form.addEventListener("submit", validateForm, false);
 
 function validateForm(event) {
@@ -30,7 +28,6 @@ function validateForm(event) {
 **/
 function validateInput(field_name, input) {
    if (input.value == '' || input.value == null) {
-      console.log(input.value)
       // get the error div next to each input element
       const errorDiv = input.nextElementSibling;
       input.classList.add("danger");
@@ -59,21 +56,25 @@ function validateInput(field_name, input) {
             }
             break;
          case "Check In":
-            alert("We outside!");
-            console.log(input.value);
-            const date_re = new RegExp("[0-9]{4}-[0-1][0-2]-[0-3][0-9]", "g");
-            if (date_re.exec(input.value) != null) {
-               addError(input, "Invalid date");
+            console.log(input)
+            if (new Date(input.value) == "Invalid Date") {
+               addError(input, "Invalid Date");
+            } else {
+               input.classList.remove("danger");
+               input.nextElementSibling.classList.remove("show-error");
+               input.nextElementSibling.classList.add("hidden");
             }
             break;
          case "Check Out":
-            alert("Oka")
-            const co_re = new RegExp("[0-9]{4}-[0-1][0-2]-[0-3][0-9]", "g");
-            if (co_re.exec(input.value) != null) {
-               addError(input, "Invalid date");
-               check(input);
+            console.log(input)
+            if (new Date(input.value) == "Invalid Date") {
+               addError(input, "Invalid Date");
+            } else if (new Date(checkIn.value) >= new Date(input.value)) {
+               addError(input, "You can only check out on a later date");
             } else {
-               check(input);
+               input.classList.remove("danger");
+               input.nextElementSibling.classList.remove("show-error");
+               input.nextElementSibling.classList.add("hidden");
             }
             break;
       }
@@ -81,12 +82,10 @@ function validateInput(field_name, input) {
 }
 
 // removes error message and make input normal when user starts to type
-function check(event) {
-   if (this.value.trim() != "" || this.value != null) {
-      this.classList.remove("danger");
-      this.nextElementSibling.classList.remove("show-error");
-      this.nextElementSibling.classList.add("hidden");
-   }
+function check() {
+   this.classList.remove("danger");
+   this.nextElementSibling.classList.remove("show-error");
+   this.nextElementSibling.classList.add("hidden");
 }
 
 // displays error message and makes input red by adding the danger class
